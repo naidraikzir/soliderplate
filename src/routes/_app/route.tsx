9362f683b8a7e7298846171d62c7cbe075cd2bb1
Rizki Ardian <radeite8@gmail.com>
2026-03-13 18:26:25 +0700

    add link to form

--- src/routes/_app/route.tsx
+++ src/routes/_app/route.tsx
@@ -1,5 +1,5 @@
 import { useColorMode } from '@kobalte/core'
-import { createFileRoute, Outlet, redirect } from '@tanstack/solid-router'
+import { createFileRoute, Link, Outlet, redirect } from '@tanstack/solid-router'
 
 import { Button } from '@/components/ui/button'
 import { logout } from '@/modules/auth/services'
@@ -29,6 +29,7 @@ function RouteComponent() {
     <main class="grid gap-2 p-2">
       <header class="flex items-center gap-4">
         <Button onClick={onLogout}>Logout</Button>
+        <Link to="/form-example">Form Example</Link>
         <Button variant="ghost" size="icon" onClick={toggleColorMode}>
           <span class="[html[data-kb-theme=light]_&]:icon-[lucide--sun] [html[data-kb-theme=dark]_&]:icon-[lucide--moon]" />
         </Button>
