40ee981879941871690c7ef2cca8f780b885ee56
Rizki Ardian <radeite8@gmail.com>
2026-03-13 18:23:48 +0700

    migrate to vite+

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import solidDevtools from 'solid-devtools/vite'
-import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
+  lint: { options: { typeAware: true, typeCheck: true } },
+  fmt: {
+    semi: false,
+    singleQuote: true,
+    experimentalSortImports: {},
+    ignorePatterns: ['src/components/ui/**'],
+  },
+  staged: {
+    '*.{ts,tsx}': ['vp lint --type-aware', 'vp fmt --no-error-on-unmatched-pattern'],
+  },
   plugins: [
     tailwindcss(),
     tanstackRouter({
