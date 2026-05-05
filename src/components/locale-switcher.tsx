13ee70e355f35ed60417e3e54a5b2c93d2da1e0e
Rizki Ardian <radeite8@gmail.com>
2026-05-05 13:43:10 +0700

    fix: header style

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,18 +1,35 @@
+import { For } from 'solid-js'
+
+import { cx } from '@/lib/cva'
 import { m } from '@/paraglide/messages'
-import { getLocale, setLocale } from '@/paraglide/runtime'
+import { getLocale, locales, setLocale } from '@/paraglide/runtime'
 
 import { Button } from './ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 
 export function LocaleSwitcher(props: { class?: string }) {
   return (
-    <Button
-      variant="ghost"
-      class={props.class}
-      onClick={() =>
-        confirm(m.change_locale()) ? setLocale(getLocale() === 'en' ? 'id' : 'en') : null
-      }
-    >
-      {getLocale()}
-    </Button>
+    <Popover>
+      <PopoverTrigger class={props.class}>
+        <Button variant="ghost" size="icon-sm">
+          {getLocale()}
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent class="w-auto p-1">
+        <For each={locales}>
+          {(locale) => (
+            <Button
+              variant="ghost"
+              size="icon-sm"
+              class={cx('block border', { 'opacity-50 border-0': locale !== getLocale() })}
+              onClick={() => (confirm(m.change_locale()) ? setLocale(locale) : null)}
+            >
+              {locale}
+            </Button>
+          )}
+        </For>
+      </PopoverContent>
+    </Popover>
   )
 }
--- src/routes/_app/route.tsx
+++ src/routes/_app/route.tsx
@@ -29,7 +29,7 @@ function RouteComponent() {
 
   return (
     <main class="grid grid-cols-1 gap-2 p-2">
-      <header class="flex items-center gap-4">
+      <header class="flex items-center justify-between gap-4">
         <div class="flex gap-2">
           <For each={links}>
             {({ to, label }) => (
@@ -40,11 +40,16 @@ function RouteComponent() {
           </For>
         </div>
 
-        <LocaleSwitcher class="ml-auto" />
-        <Button variant="ghost" size="icon" onClick={toggleColorMode}>
-          <span class="[html[data-kb-theme=light]_&]:icon-[lucide--sun] [html[data-kb-theme=dark]_&]:icon-[lucide--moon]" />
-        </Button>
-        <Button onClick={onLogout}>Logout</Button>
+        <div class="flex items-center gap-2">
+          <LocaleSwitcher />
+          <Button variant="ghost" size="icon-sm" onClick={toggleColorMode}>
+            <span class="[html[data-kb-theme=light]_&]:icon-[lucide--sun] [html[data-kb-theme=dark]_&]:icon-[lucide--moon]" />
+          </Button>
+          <Button onClick={onLogout}>
+            <span class="max-sm:icon-[lucide--log-out]" />
+            <span class="hidden sm:inline">Logout</span>
+          </Button>
+        </div>
       </header>
 
       <div class="w-2xl max-w-full mx-auto">
